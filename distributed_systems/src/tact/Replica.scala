e829e2ba20119757658c0b3568e47a800556d2db
Paul <paulverkooijen93@gmail.com>
2019-03-05 11:28:52 +0100

    Added all database functions

--- distributed_systems/src/tact/Replica.scala
+++ distributed_systems/src/tact/Replica.scala
@@ -13,7 +13,7 @@ class  Replica {
   private var writeLog = Nil
 
   /** Will contain all conits, one for each DB entry **/
-  private var conits = Map[Char, Conit] = Map()
+  private var conits = Map[Char, Conit]()
 
   /** The consistency manager will keep track of all error variables in the replica **/
   private var consistencyManager = new ConsistencyManager {}
