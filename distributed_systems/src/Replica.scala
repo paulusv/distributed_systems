8c209cc171d4ff331554acfff4c1b3b6051f95c2
Paul <paulverkooijen93@gmail.com>
2019-03-05 10:36:52 +0100

    small addition

--- distributed_systems/src/Replica.scala
+++ distributed_systems/src/Replica.scala
@@ -1,16 +1,16 @@
 class  Replica {
 
   /** Each replica has a database, which will be updated by other replicas via the consistency manager **/
-  var dataBase = new DataBase {}
+  private var dataBase = new DataBase {}
 
   /** The writelog contains all writes that are made **/
-  var writeLog = Nil
+  private var writeLog = Nil
 
   /** Will contain all conits, one for each DB entry **/
-  var conits = Map[Char, Conit] = Map()
+  private var conits = Map[Char, Conit] = Map()
 
   /** The consistency manager will keep track of all error variables in the replica **/
-  var consistencyManager = new ConsistencyManager {}
-  
+  private var consistencyManager = new ConsistencyManager {}
+
 
 }
