1755f2ad180939949df78fefa2875436379986d3
nvanruyven <noor_van_ruyven@hotmail.com>
2019-03-13 15:55:30 +0100

    Nog wat dingetjes

--- src/main/scala/tact/manager/ConsistencyManager.scala
+++ src/main/scala/tact/manager/ConsistencyManager.scala
@@ -9,6 +9,7 @@ class ConsistencyManager(replica: Replica) {
   var numericalError: Int = 0
   var orderError: Int = 0
   var logicalTimeVector: Int = 0
+  var ECG: Replica = null
 
 
   def nweight(W: WriteLogItem, F: Conit): Int ={
@@ -26,9 +27,9 @@ class ConsistencyManager(replica: Replica) {
     else {
       0
     }
-
   }
 
+  // Finds the longest common prefix of history 1 and history 2
   def getPrefix(H1: WriteLog, H2: WriteLog) ={
     var count = 0
     for (i <- 0 to H1.writeLogItems.size){
@@ -40,6 +41,24 @@ class ConsistencyManager(replica: Replica) {
       }
     }
     count
+  }
+
+  // Numerical Error (absolute) = F(D_ideal) - F(D_observed)
+  def calculateNumerical_absolute(F: Conit) ={
+    var con = new Conit(0,0)
+    ECG.getOrCreateConit(F.getKey).getValue - replica.conits.getOrElse(F.getKey, con).getValue
+  }
+
+  // Numerical Error (relative) = 1 - F(D_observed)/F(D_ideal)
+  def calculateNumerical_relative(F: Conit) ={
+    var con = new Conit(0,0)
+    1 - (replica.conits.getOrElse(F.getKey, con).getValue)/(ECG.getOrCreateConit(F.getKey).getValue)
+  }
+
+  // Alvast de commands voor de tijd opgezocht
+  def staleness() ={
+    var stime = System.nanoTime()
+    var rtime = System.nanoTime()
   }
 
 }
